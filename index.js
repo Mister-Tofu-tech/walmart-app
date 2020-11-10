217f8b00a325a7023c0a53ed356b2de300a28cab
Jakin Wang <darkfact247@gmail.com>
2020-11-10 00:07:23 -0800

    Completed

--- index.js
+++ index.js
@@ -4,31 +4,35 @@ const ejs = require("ejs");
 const fetch = require('node-fetch');
 const { json } = require('express');
 
+// Fetch json data and store it in json_data
 var json_data;
-fetch('https://api.github.com/repos/walmartlabs/thorax/issues')
+var org = "walmartlabs", repo = "thorax";
+fetch(`https://api.github.com/repos/${org}/${repo}/issues`)
     .then(response => response.json())
     .then(data => {
         json_data = data;
     })
     .catch(e => console.error(e));
 
+// Declare the public folder that we need
 app.use(express.static("public"));
+// Set Up template engine ejs
 app.set("view engine", "ejs");
 
+// Each route for the browser
 app.get("/", (req, res) => {
-    var page_num = json_data.length / 10;
-    console.log(page_num);
     res.render("index", { data: json_data, num: 1 });
 });
 
+
 app.get("/:num", (req, res) => {
-    res.render("index", { data: json_data, num: req.params.num });
+    var page_number = req.params.num;
+    res.render("index", { data: json_data, num: page_number });
 });
     
 app.get("/issue/:num", (req, res) => {
-    res.render("issue", { issue: json_data[req.params.num] });
+    var page_number = req.params.num;
+    res.render("issue", { issue: json_data[page_number], num: page_number});
 });
 
-app.listen(3000, () => {
-    console.log("App listening at 3000!!");
-});
+app.listen( process.env.PORT || 3000, () => {});
